80be958cf4524ebc9bae0ddc2b5f96e40247014a
loomhigh <45379144+loomhigh@users.noreply.github.com>
2025-06-01 13:35:07 +0800

    Update syncflake.sh
    resets local file to prevent error about local changes

--- syncflake.sh
+++ syncflake.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-exec git -C ~/Flakes/ pull origin main
+exec git reset --hard main ## resets the git file to remove any local changes and thus allow git to replace it without error
+exec git -C ~/Flakes/ pull origin main #Does the deed
 
 end

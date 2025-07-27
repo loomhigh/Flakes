40aa64a6f529ec7da44c385fe98015fcc6da2740
loomhigh <loomhigh223555@gmail.com>
2025-07-27 18:22:07 +0800

    modified:   configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -12,8 +12,15 @@
 
 
   # Bootloader.
-  boot.loader.grub.devices = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+    # old systemd version
+    #boot.loader.systemd-boot.enable = true;
+    #boot.loader.efi.canTouchEfiVariables = true;
+
+ boot.loader.systemd-boot.enable = false;
+boot.loader.grub.enable = true;
+boot.loader.grub.device = "nodev";
+boot.loader.grub.efiSupport = true;
+boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "truebeliever"; # Define your hostname.
   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.

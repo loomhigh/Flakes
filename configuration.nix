0eb49eddd3fef342adbc310e39e4b367b11db769
loomhigh <loomhigh223555@gmail.com>
2025-07-27 19:42:25 +0800

    modified:   configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
-  hardware.enableAllHardware = true
+  hardware.enableAllHardware = true;
 
   # Bootloader.
     # old systemd version

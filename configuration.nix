68e5f8ba950c8e1badf5acdb7eb13829cb001665
loomhigh <loomhigh223555@gmail.com>
2025-07-27 18:14:39 +0800

    replace SysteMD with grub

--- configuration.nix
+++ configuration.nix
@@ -12,7 +12,7 @@
 
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "truebeliever"; # Define your hostname.

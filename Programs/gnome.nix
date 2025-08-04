d2ab3544c4e3d098cdad7de516ef1f09e7d835f8
loomhigh <loomhigh223555@gmail.com>
2025-08-04 15:05:59 +0800

    modified:   Programs/gnome.nix

--- Programs/gnome.nix
+++ Programs/gnome.nix
@@ -8,8 +8,8 @@
       accent-color = "blue";
     };
     "/org/gnome/desktop/background" = {
-      #picture-uri = "file:///etc/nixos/Profiles/Campaigner/background.jpg";
-      picture-uri = "file:///home/truebeliever/Documents/background.jpg";
+      picture-uri = 'file:///etc/nixos/Profiles/Campaigner/background.jpg';
+      #picture-uri = "file:///home/truebeliever/Documents/background.jpg";
     };
   };
 }

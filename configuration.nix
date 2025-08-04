77725c3ee205dad042fe11f8fcfb33e85d10ce54
loomhigh <loomhigh223555@gmail.com>
2025-08-04 14:10:07 +0800

    modified:   configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -90,11 +90,6 @@
    # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  home.packages = with pkgs; [
-
-    gnome-tweaks # a way of messing with the settings of gnome better
-
-  ];
   environment.gnome.excludePackages = with pkgs; [
     totem
     geary
@@ -138,6 +133,7 @@
   environment.systemPackages = with pkgs; [
   btop # System monitor you can run in Terminal, I love this thing
   fish # better than bash IMO.
+  gnome-tweaks
   git # Main way of updating the software
   nh # I genuinely cannot remember what this does
   micro # Like the nano text editor, but a little more advamced

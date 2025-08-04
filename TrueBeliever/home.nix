271dde3c35b740429d7f9d23ccf24e6710d81098
loomhigh <loomhigh223555@gmail.com>
2025-08-04 12:08:40 +0800

    modified:   TrueBeliever/home.nix

--- TrueBeliever/home.nix
+++ TrueBeliever/home.nix
@@ -1,6 +1,31 @@
+ { config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "truebeliever";
+  home.homeDirectory = "/home/truebeliever";
+
+
   # User packages
   home.packages = with pkgs; [
    fastfetch
   
   
   ];
+
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "25.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
+

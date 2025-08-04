713d9dd0983ec84f22afe2f31895a4b6dc0a6ea6
loomhigh <loomhigh223555@gmail.com>
2025-08-04 12:05:24 +0800

    renamed:    truebeliever/home.nix -> TrueBeliever/home.nix modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.truebeliever = ./home.nix;
+            home-manager.users.truebeliever = ./TrueBeliever/home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix

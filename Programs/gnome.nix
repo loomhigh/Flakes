88a2a069425f2f5eac50a025d5c6e7d7856cbf35
loomhigh <loomhigh223555@gmail.com>
2025-08-04 14:08:44 +0800

    modified:   Programs/gnome.nix

--- Programs/gnome.nix
+++ Programs/gnome.nix
@@ -1,14 +1,6 @@
 {config, pkgs, lib, ...}:
 
 {
-   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  home.packages = with pkgs; [
-
-    gnome-tweaks # a way of messing with the settings of gnome better
-
-  ];
   
   dconf.enable = true;
   dconf.settings = {
--- configuration.nix
+++ configuration.nix
@@ -87,7 +87,14 @@
     ];
     shell = pkgs.fish;
   };
+   # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  home.packages = with pkgs; [
 
+    gnome-tweaks # a way of messing with the settings of gnome better
+
+  ];
   environment.gnome.excludePackages = with pkgs; [
     totem
     geary

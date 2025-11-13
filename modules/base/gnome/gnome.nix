ede99f37c75333415405e32edd1099ec78c8a66a
loomhigh <loomhigh223555@gmail.com>
2025-11-13 15:19:03 +0800

    gnome stuff

--- modules/base/gnome/gnome.nix
+++ modules/base/gnome/gnome.nix
@@ -1,9 +1,24 @@
 {config, pkgs, lib, ...}:
 
 {
+   # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  environment.gnome.excludePackages = with pkgs; [
+    totem
+    geary
+    gnome-tour
+    gnome-maps
+    gnome-music
+    gnome-mines
+    gnome-chess
+    gnome-sudoku
+    gnome-robots
+    gnome-weather
+  ];
   
-  dconf.enable = true;
-  dconf.settings = {
+  programs.dconf.enable = true;
+  programs.dconf.settings = {
     "org/gnome/desktop/interface" = {
       accent-color = "blue";
     };

6e7e3179539d087cb9f5824b12cb3a72f0d873be
loomhigh <loomhigh223555@gmail.com>
2025-11-13 15:27:11 +0800

    if IDK what it is its getting deleted

--- hosts/TrueBeliever/default.nix
+++ hosts/TrueBeliever/default.nix
@@ -5,9 +5,4 @@
     ./configuration.nix
   ];
 
-  config = {
-    home-manager.users = builtins.listToAttrs
-      (map (user: { name = user; value =
-                      { imports = [ ./home.nix ../../modules/user ]; };}) config.systemSettings.users);
-    };
 }

c74c28b05b69a4809cf4343f46d1cf60f7d62ff6
loomhigh <loomhigh223555@gmail.com>
2025-11-13 15:24:52 +0800

    idk

--- hosts/TrueBeliever/home.nix
+++ hosts/TrueBeliever/home.nix
@@ -2,7 +2,6 @@
 
 {
   imports = [
-    ../../Programs/gnome.nix
 
   ];
   # Home Manager needs a bit of information about you and the
@@ -33,5 +32,18 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-}
 
+# Gnome customizations
+dconf.enable = true;
+dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      accent-color = "blue";
+    };
+    "org/gnome/desktop/background" = {
+      #color-shading-type = "solid";
+      picture-uri = "file:///etc/nixos/Profiles/Campaigner/background.jpg";
+      #picture-uri = "file:///home/truebeliever/Documents/background.jpg";
+    };
+  };
+
+}

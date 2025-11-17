4f9fa8a6c24730180cc92d2a12c3c186f68b8444
loomhigh <loomhigh223555@gmail.com>
2025-11-17 13:03:51 +0800

    trying to add dualboot

--- hosts/Personal/configuration.nix
+++ hosts/Personal/configuration.nix
@@ -17,11 +17,25 @@
   boot.loader = {
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
-    grub = {
+    systemd-boot = {
       enable = true;
-      devices = [ "nodev" ];
-      efiSupport = true;
-      useOSProber = true;
+      windows = {
+        "windows" =
+          let
+            # To determine the name of the windows boot drive, boot into edk2 first, then run
+            # `map -c` to get drive aliases, and try out running `FS1:`, then `ls EFI` to check
+            # which alias corresponds to which EFI partition.
+            boot-drive = "FS1";
+          in
+          {
+            title = "Windows";
+            efiDeviceHandle = boot-drive;
+            sortKey = "y_windows";
+          };
+      };
+
+      edk2-uefi-shell.enable = true;
+      edk2-uefi-shell.sortKey = "z_edk2";
     };
   };
 

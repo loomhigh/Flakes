7f5ddc04bc9e16cf4d87f00287e6665b173dc99a
loomhigh <loomhigh223555@gmail.com>
2025-11-17 15:22:55 +0800

    added nix language to extensions

--- modules/development/vscodium/vscodium.nix
+++ modules/development/vscodium/vscodium.nix
@@ -13,7 +13,12 @@ environment.systemPackages = with pkgs; [
         version = "0.120.2";
         sha256 = "0gi9ib9cf5h7p3l1mxs1z4wjv69kwjxngw6fz071362q7215fp1g";
       }
-
+      {
+        name = "nix-ide";
+        publisher = "jnoortheen";
+        version = "0.5.0";
+        sha256 = "1cpfckh6zg8byi6x1llkdls24w9b0fvxx4qybi9zfcy5gc60r6nk";
+      }
     ];
   })
 ];

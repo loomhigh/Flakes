78b16bedd069e28f8145dae22f925ded7c6325c1
loomhigh <loomhigh223555@gmail.com>
2025-11-22 14:19:12 +0800

    expecting ; moment

--- hosts/Entertainer/configuration.nix
+++ hosts/Entertainer/configuration.nix
@@ -41,7 +41,7 @@ xdg.terminal-exec = {
   settings = {
     default = [ "kodi" ];
   };
-}
+};
   #services.xserver.desktopManager.kodi.enable = true;
   services.displayManager.autoLogin.user = "entertainer";
   services.xserver.displayManager.lightdm.greeter.enable = false;

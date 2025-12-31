8cac7da25a9ad613d984e96107c6c6635fd41540
loomhigh <loomhigh223555@gmail.com>
2025-12-31 13:28:02 +0800

    adding a JRE (Jameica)

--- modules/office/libreoffice/davinci-resolve-studio.nix
+++ modules/office/libreoffice/davinci-resolve-studio.nix
@@ -3,6 +3,7 @@
 {
   environment.systemPackages = with pkgs; [
   libreoffice-qt-fresh # QT Fixes a plasma bug and fresh just means latest stable version
+  jameica # Java Runtime Environment for libreoffice base
   ];
 
 }

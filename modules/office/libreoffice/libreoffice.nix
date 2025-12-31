60687607c77b0a88df71e4f601f5c77cc7468e68
loomhigh <loomhigh223555@gmail.com>
2025-12-31 14:33:37 +0800

    replace qt with plain fresh

--- modules/office/libreoffice/libreoffice.nix
+++ modules/office/libreoffice/libreoffice.nix
@@ -2,7 +2,7 @@
 
 {
   environment.systemPackages = with pkgs; [
-  libreoffice-qt-fresh # QT Fixes a plasma bug and fresh just means latest stable version
+  libreoffice-fresh # fresh just means latest stable version
   jdk21_headless # Java so libreoffice base will work
   ];
 

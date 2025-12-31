f7813d1544d23bd71bdad00da921c13b0ffabf1d
loomhigh <loomhigh223555@gmail.com>
2025-12-31 14:51:27 +0800

    change jdk to openjdk25

--- modules/office/libreoffice/libreoffice.nix
+++ modules/office/libreoffice/libreoffice.nix
@@ -5,7 +5,7 @@
   libreoffice-fresh # fresh just means latest stable version
   hunspell # For spellcheck
   hunspellDicts.en_AU # Aussie spellings
-  jdk8_headless # Java so libreoffice base will work
+  javaPackages.compiler.openjdk25 # Java so libreoffice base will work
   ];
 
 }

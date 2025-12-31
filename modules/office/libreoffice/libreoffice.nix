d8e032a642a92702140b77ff0a8699c28d1caefb
loomhigh <loomhigh223555@gmail.com>
2025-12-31 15:39:51 +0800

    testing base without libreoffice

--- modules/office/libreoffice/libreoffice.nix
+++ modules/office/libreoffice/libreoffice.nix
@@ -5,8 +5,9 @@
   libreoffice-fresh # fresh just means latest stable version
   hunspell # For spellcheck
   hunspellDicts.en_AU # Aussie spellings
-  jdk25_headless
-  #Can't seem to get libreoffice base to work due to Java Runtime Issues
+  #jdk25_headless
+  #haven't tested if libreoffice base can function without jdk25
+  # For future reference, if libreoffice base crashes, delete configs with rm -rf ~/.config/libreoffice
   ];
 
 }

ac66ea3221d4eee765c7659fbb0dd16461573c3e
loomhigh <loomhigh223555@gmail.com>
2026-01-18 21:00:57 +0800

    fix activations cript

--- modules/extra/kodi/kodi.nix
+++ modules/extra/kodi/kodi.nix
@@ -8,11 +8,11 @@ environment.systemPackages = [
 	]))
 ];
 
-system.activationScripts.lutriskodi.text = ''
-
-$wget -T 5 -P /home/Entertainer-nc https://github.com/solbero/repository.solbero.matrix/raw/master/repository.solbero.matrix/repository.solbero.matrix.zip
-
-''
+system.activationScripts.lutriskodi = {
+	text = ''
+wget -T 5 -P /home/Entertainer-nc https://github.com/solbero/repository.solbero.matrix/raw/master/repository.solbero.matrix/repository.solbero.matrix.zip
+'';
+}
  # need to figure out how to get this file downloaded 
  # https://github.com/solbero/repository.solbero.matrix/raw/master/repository.solbero.matrix/repository.solbero.matrix.zip
 

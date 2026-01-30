1cca6c0c9e0639f0c6c1d08c779758ede3b81f31
loomhigh <loomhigh223555@gmail.com>
2026-01-30 19:14:33 +0800

    lib32 drivers?

--- modules/extra/lutris/lutris.nix
+++ modules/extra/lutris/lutris.nix
@@ -8,6 +8,7 @@ environment.systemPackages = with pkgs; [
 	#wine64
 	wineWowPackages.stable ##Let Lutris handle windows compatibility tbh
 	winetricks
+	driversi686Linux.mesa #Drivers to run bdl2?
 ];
 
 }

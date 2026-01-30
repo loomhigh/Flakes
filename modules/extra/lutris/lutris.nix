eb3d7dc93906f3aa3932469f54c4cbe0e46195b1
loomhigh <loomhigh223555@gmail.com>
2026-01-30 18:55:04 +0800

    readding wine

--- modules/extra/lutris/lutris.nix
+++ modules/extra/lutris/lutris.nix
@@ -6,8 +6,8 @@ environment.systemPackages = with pkgs; [
 	
 	lutris
 	#wine64
-	#wineWowPackages.stable ##Let Lutris handle windows compatibility tbh
-	#winetricks
+	wineWowPackages.stable ##Let Lutris handle windows compatibility tbh
+	winetricks
 ];
 
 }

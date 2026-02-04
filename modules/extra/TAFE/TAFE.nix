9a4ebc583fccab12022c130badb18a14b0eccf62
loomhigh <loomhigh223555@gmail.com>
2026-02-04 11:30:38 +0800

    enabling vmware

--- modules/extra/TAFE/TAFE.nix
+++ modules/extra/TAFE/TAFE.nix
@@ -1,7 +1,7 @@
 {config, pkgs, lib, ...}:
 
 {
-
+virtualisation.vmware.host.enable = true;
 environment.systemPackages = with pkgs; [
 	
 vmware-workstation #virtual machine manager required by TAFE

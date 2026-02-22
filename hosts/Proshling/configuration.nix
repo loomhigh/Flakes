3609cc42634b43c4b915e5322d8110547c36a94e
loomhigh <loomhigh223555@gmail.com>
2026-02-22 19:06:50 +0800

    adding script dependencies

--- hosts/Proshling/configuration.nix
+++ hosts/Proshling/configuration.nix
@@ -50,11 +50,18 @@
   services.displayManager.autoLogin.user = "Proshling";
 
   system.activationScripts.hmlsetup = {
+
+  runtimeInputs = [
+    wget
+    unzip
+  ];
+  
 	text = ''
   cp -R /etc/nixos/hosts/Proshling/configs/Wallpapers /home/Proshling/
   cp -R /etc/nixos/hosts/Proshling/configs/color-schemes /home/Proshling/.local/share/
   mkdir /home/Proshling/.local/share/plasma
   mkdir /home/Proshling/.local/share/plasma/look-and-feel
+  chown -R Proshling /home/Proshling/.local/share/plasma
   wget -T 5 -P /home/Proshling/.local/share/plasma/look-and-feel -nc https://codeberg.org/esm/GangsterEdition/archive/main.zip
   unzip /home/Proshling/.local/share/plasma/look-and-feel/temp.zip
   rm /home/Proshling/.local/share/plasma/look-and-feel/temp.zip

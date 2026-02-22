249208a3abfe14d80d2a8531cffc60fcc9da9e81
loomhigh <loomhigh223555@gmail.com>
2026-02-22 15:05:06 +0800

    fixing capitalisation floor in non-used hosts.

--- hosts/Entertainer/home.nix
+++ hosts/Entertainer/home.nix
@@ -3,4 +3,14 @@
 {
  home.stateVersion = "26.05";
 
+/* BEFORE USING HOME-MANAGER
+
+For this host you need to fix the username problem.
+You accidentally configured each host username with uncapitalised letters when the folder is capitalised
+in this example. the host 'Entertainer' has the username 'entertainer' when it should be 'Entertainer'
+Fixing this issue is going to mess with any manually configured settings on the device and so should only be done with
+the time and dedication to fix those issues. Make sure you don't need to use this device soon if you do this.
+- loomhigh 22/02/2025
+
+*/
 }
--- hosts/Personal/home.nix
+++ hosts/Personal/home.nix
@@ -3,4 +3,15 @@
 {
  home.stateVersion = "26.05";
 
+/* BEFORE USING HOME-MANAGER
+
+For this host you need to fix the username problem.
+You accidentally configured each host username with uncapitalised letters when the folder is capitalised
+in this example. the host 'Personal' has the username 'personal' when it should be 'Personal'
+Fixing this issue is going to mess with any manually configured settings on the device and so should only be done with
+the time and dedication to fix those issues. Make sure you don't need to use this device soon if you do this.
+- loomhigh 22/02/2025
+
+*/
+
 }

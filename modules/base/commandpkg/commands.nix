e913a5eacc4fa48861d0f0f167216a8f9db49793
loomhigh <loomhigh223555@gmail.com>
2026-02-22 18:30:22 +0800

    rearrange

--- modules/base/commandpkg/commands.nix
+++ modules/base/commandpkg/commands.nix
@@ -5,14 +5,15 @@
   wget
   usbutils
   yt-dlp
+  traceroute #allows you to see where your pings travel through
+  unzip #allows me to use the unzip and zip command
 
 # Read Hardware info like GPUs and stuff
   inxi
   lshw 
   pciutils
   virtualgl # contains glxinfo
-  traceroute #allows you to see where your pings travel through
-  unzip #allows me to use the unzip and zip command
+  
   ];
 
 }

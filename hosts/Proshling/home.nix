576fa488f47679333870e0193f58ea0960273408
loomhigh <loomhigh223555@gmail.com>
2026-02-22 14:31:11 +0800

    forgor slash

--- hosts/Proshling/home.nix
+++ hosts/Proshling/home.nix
@@ -9,7 +9,7 @@ programs.plasma = {
     enable = true;
 
   workspace = {
-    wallpaper = "etc/nixos/hosts/Proshling/background.jpg";
+    wallpaper = "/etc/nixos/hosts/Proshling/background.jpg";
     #lookAndFeel = "Plasma-Overdose";
   };  
 };

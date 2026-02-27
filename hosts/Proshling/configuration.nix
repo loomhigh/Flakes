be4cfecee5badc5ddd5f97fa70037b6143490b72
loomhigh <loomhigh223555@gmail.com>
2026-02-27 16:55:22 +0800

    adding gnutar as dependency

--- hosts/Proshling/configuration.nix
+++ hosts/Proshling/configuration.nix
@@ -23,7 +23,7 @@
   ];
     services.hmlinux = {
       path = with pkgs; [
-      xorg.setxkbmap
+      gnutar
       ];
       script = ''
       cp -R /etc/nixos/hosts/Proshling/configs/Wallpapers /home/Proshling/

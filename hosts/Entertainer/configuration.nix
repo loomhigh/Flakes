d94fdf93ae9546b050561875844038daf381ed89
loomhigh <loomhigh223555@gmail.com>
2026-03-10 20:29:22 +0800

    bluetooth

--- hosts/Entertainer/configuration.nix
+++ hosts/Entertainer/configuration.nix
@@ -68,6 +68,30 @@ xdg.terminal-exec = {
   services.displayManager.autoLogin.user = "entertainer";
   services.xserver.displayManager.lightdm.greeter.enable = false;
 
+  #bluetooth
+  services.blueman.enable = true;
+  hardware.bluetooth = {
+  enable = true;
+  powerOnBoot = true;
+  settings = {
+    General = {
+      # Shows battery charge of connected devices on supported
+      # Bluetooth adapters. Defaults to 'false'.
+      Experimental = true;
+      # When enabled other devices can connect faster to us, however
+      # the tradeoff is increased power consumption. Defaults to
+      # 'false'.
+      FastConnectable = true;
+    };
+    Policy = {
+      # Enable all controllers when they are found. This includes
+      # adapters present on start as well as adapters that are plugged
+      # in later on. Defaults to 'true'.
+      AutoEnable = true;
+    };
+  };
+};
+
   # Install firefox and set default shell to fish
   users.defaultUserShell = pkgs.fish;
   programs = {

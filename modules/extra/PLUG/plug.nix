26ec59bdb4c68086d1512d2dabed502902dd67d1
loomhigh <loomhigh223555@gmail.com>
2026-03-15 20:53:31 +0800

    removed Ical

--- modules/extra/PLUG/plug.nix
+++ modules/extra/PLUG/plug.nix
@@ -9,7 +9,7 @@ ansible #unstable is the only one that seems to have a version passt 2.2
 
 python313 # Python3
 python313Packages.dateutils #added for PLUG
-python313Packages.ical
+#python313Packages.ical #
 python313Packages.icalendar 
 ];
 

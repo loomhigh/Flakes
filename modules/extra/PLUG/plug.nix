4f5ffad1d12cc4e7a43f23a5e44ed53ac35e0885
loomhigh <loomhigh223555@gmail.com>
2026-03-17 15:28:51 +0800

    see if I can upgrade python

--- modules/extra/PLUG/plug.nix
+++ modules/extra/PLUG/plug.nix
@@ -7,7 +7,7 @@ environment.systemPackages = with pkgs; [
 sshpass
 ansible #unstable is the only one that seems to have a version passt 2.2
 
-  (python3.withPackages (python-pkgs: with python-pkgs; [
+  (python4.withPackages (python-pkgs: with python-pkgs; [
       dateutils
       icalendar
       ical

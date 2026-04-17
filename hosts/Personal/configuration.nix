8fb2af2a96efca68975400bdf48f4456e250647c
loomhigh <loomhigh223555@gmail.com>
2026-04-17 22:37:56 +0800

    trial and error the bugs

--- hosts/Personal/configuration.nix
+++ hosts/Personal/configuration.nix
@@ -46,11 +46,13 @@
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
     # of just the bare essentials.
-    powerManagement.enable = false;
+    #temporarily commenting out
+    #powerManagement.enable = false;
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+    #temporarily commenting out
+    #powerManagement.finegrained = false;
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
@@ -65,7 +67,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    #package = config.boot.kernelPackages.nvidiaPackages.stable;
     #package = config.boot.kernelPackages.nvidiaPackages.legacy_535; # Older versions
     
     # Prime

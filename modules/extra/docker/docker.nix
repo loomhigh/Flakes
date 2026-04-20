e9db0b2bf1bfb485ea1dd43b0bade95b7d192ee3
loomhigh <loomhigh223555@gmail.com>
2026-04-20 21:27:59 +0800

    absulte files file absolutelyt

--- modules/extra/docker/docker.nix
+++ modules/extra/docker/docker.nix
@@ -11,6 +11,6 @@
     enable = true;
     hostname = "monica.campaigndecentral.ch";
     appURL = "localhost:8080";
-    appKeyFile = "Q7qGf3xh9YkJzTStq0VKvhkwZGNhlXsVJpLC3iF5hOI=";
+    appKeyFile = /etc/nixos/hosts/modules/extra/docker/appkeyidk
   };
 }

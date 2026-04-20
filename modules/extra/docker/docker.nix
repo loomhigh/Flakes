da9958f34b7181c72684237daebb19a856325d20
loomhigh <loomhigh223555@gmail.com>
2026-04-20 21:44:33 +0800

    appkeuFILWE

--- modules/extra/docker/docker.nix
+++ modules/extra/docker/docker.nix
@@ -11,6 +11,6 @@
     enable = true;
     hostname = "monica.campaigndecentral.ch";
     appURL = "localhost:8080";
-    appKey = "/etc/nixos/modules/extra/docker/appkeyidk";
+    appKeyFile = "/etc/nixos/modules/extra/docker/appkeyidk";
   };
 }

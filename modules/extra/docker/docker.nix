fc175182dffd23d485e980b87484eb6150c5d9c7
loomhigh <loomhigh223555@gmail.com>
2026-04-20 22:56:57 +0800

    I trust myself

--- modules/extra/docker/docker.nix
+++ modules/extra/docker/docker.nix
@@ -12,14 +12,19 @@
   services.monica = {
     enable = true;
     database.createLocally = true;
-   # appKeyFile = "/etc/nixos/modules/extra/docker/appkeyidk";
+    appKeyFile = "/etc/nixos/modules/extra/docker/appkeyidk";
   
+
+
   nginx = { 
     forceSSL = true;
+    sslCertificate = "/etc/nixos/modules/extra/docker/cert.pem";
+    sslCertificateKey = "/etc/nixos/modules/extra/docker/key.pem";
+    sslTrustedCertificate = "/etc/nixos/modules/extra/docker/cert.pem";
     listen = [ { 
       addr = "127.0.0.1"; 
-      #port = 8000; 
-      ssl = false; } ];
+      port = 8000; 
+      ssl = true; } ];
   };
   };
 }

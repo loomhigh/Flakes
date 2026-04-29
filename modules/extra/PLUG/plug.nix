617f200bba2a4d8e28d652bdc0d8d3df4ef9487c
loomhigh <loomhigh223555@gmail.com>
2026-04-29 13:06:18 +0800

    don't need this anymore

--- modules/extra/PLUG/plug.nix
+++ modules/extra/PLUG/plug.nix
@@ -3,24 +3,7 @@
 {
 
 environment.systemPackages = with pkgs; [  
-#uv
-sshpass
-ansible #unstable is the only one that seems to have a version passt 2.2
 
-  (python3.withPackages (python-pkgs: [
-      python-pkgs.uv
-      python-pkgs.dateutils
-      python-pkgs.icalendar
-      python-pkgs.ical
-      python-pkgs.markdown
-      python-pkgs.soupsieve #BeautifulSoup, converts html to txt
-      python-pkgs.beautifulsoup4
-  ]))
-#python313 # Python3
-python313Packages.dateutils #added for PLUG
-python313Packages.ical #
-python313Packages.icalendar
-#*/
 ];
 
 }

af2222b4bba5db688a78c291ab19ba643d84d0bb
loomhigh <loomhigh223555@gmail.com>
2026-04-29 10:20:16 +0800

    shell right?

--- shell.nix
+++ shell.nix
@@ -1,26 +1,48 @@
-{ 
-  pkgs, 
-  lib, 
-  stdenv, 
-  ... 
-}:
+with import <nixpkgs> { };
 
 let
-  pythonPackages = pkgs.python3Packages;
+  pythonPackages = python3Packages;
 in
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    pythonPackages.python
-    pythonPackages.venvShellHook
-  ];
+pkgs.mkShell rec {
+  name = "impurePythonEnv";
   venvDir = "./.venv";
+  buildInputs = [
+    # A Python interpreter including the 'venv' module is required to bootstrap
+    # the environment.
+    pythonPackages.python
+
+    # This executes some shell code to initialize a venv in $venvDir before
+    # dropping into the shell
+    pythonPackages.venvShellHook
+
+    # Those are dependencies that we would like to use from nixpkgs, which will
+    # add them to PYTHONPATH and thus make them accessible from within the venv.
+    pythonPackages.numpy
+    pythonPackages.requests
+
+    # In this particular example, in order to compile any binary extensions they may
+    # require, the Python modules listed in the hypothetical requirements.txt need
+    # the following packages to be installed locally:
+    taglib
+    openssl
+    git
+    libxml2
+    libxslt
+    libzip
+    zlib
+  ];
+
+  # Run this command, only after creating the virtual environment
   postVenvCreation = ''
     unset SOURCE_DATE_EPOCH
-  '';
-  postShellHook = ''
-    unset SOURCE_DATE_EPOCH
-    export LD_LIBRARY_PATH=${lib.makeLibraryPath [stdenv.cc.cc]}
     pip install -r requirements.txt
-    exit
   '';
+
+  # Now we can execute any commands within the virtual environment.
+  # This is optional and can be left out to run pip manually.
+  postShellHook = ''
+    # allow pip to install wheels
+    unset SOURCE_DATE_EPOCH
+  '';
+
 }

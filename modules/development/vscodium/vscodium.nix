292ef38ccefaf0b55d97438dd27e1121df08501f
loomhigh <loomhigh223555@gmail.com>
2026-07-20 20:30:13 +0800

    Fixing IDE extension

--- modules/development/vscodium/vscodium.nix
+++ modules/development/vscodium/vscodium.nix
@@ -8,18 +8,13 @@ environment.systemPackages = with pkgs; [
     vscodeExtensions = with vscode-extensions; [
       ms-python.python
       ms-python.debugpy
+      jnoortheen.nix-ide
     ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
       {
         name = "vscode-pull-request-github";
         publisher = "github";
         version = "0.120.2";
         sha256 = "0gi9ib9cf5h7p3l1mxs1z4wjv69kwjxngw6fz071362q7215fp1g";
-      }
-      {
-        name = "nix-ide";
-        publisher = "jnoortheen";
-        version = "0.5.0";
-        sha256 = "1cpfckh6zg8byi6x1llkdls24w9b0fvxx4qybi9zfcy5gc60r6nk";
       }
     ];
   })

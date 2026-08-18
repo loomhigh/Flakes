d5fded4a05bb8ef886737732a2ee6ca55c96dbaa
loomhigh <loomhigh223555@gmail.com>
2026-08-18 15:59:49 +0800

    using older drivers

--- hosts/Personal/configuration.nix
+++ hosts/Personal/configuration.nix
@@ -63,18 +63,18 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-    #package = config.boot.kernelPackages.nvidiaPackages.legacy_535; # Older versions
+    #package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_535; # Older versions
     
     #\ Prime
-    #dynamicBoost.enable = true;
-    #prime = {
+    dynamicBoost.enable = true;
+    prime = {
 		# Make sure to use the correct Bus ID values for your system!
-    #offload.enable = true;
-		#intelBusId = "PCI:0:2:0";
-		#nvidiaBusId = "PCI:1:0:0";
+    offload.enable = true;
+		intelBusId = "PCI:0:2:0";
+		nvidiaBusId = "PCI:1:0:0";
 		# amdgpuBusId = "PCI:54:0:0"; For AMD GPU
-	#};
+	};
   };
   #*/
   ## END OF HARDWARE STUFF

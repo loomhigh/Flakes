0082e8e518f4475a1ea0240e70240e21c69aacad
loomhigh <loomhigh223555@gmail.com>
2026-08-18 16:06:23 +0800

    screw these dang Drivers. Least favourite part of Computers

--- hosts/Personal/configuration.nix
+++ hosts/Personal/configuration.nix
@@ -56,7 +56,7 @@
     # supported GPUs is at: 
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
-    open = false;
+    #open = false;
 
     # Enable the Nvidia settings menu,
 	# accessible via `nvidia-settings`.
@@ -64,7 +64,7 @@
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     #package = config.boot.kernelPackages.nvidiaPackages.stable;
-    package = config.boot.kernelPackages.nvidiaPackages.legacy_535; # Older versions
+    #package = config.boot.kernelPackages.nvidiaPackages.legacy_535; # Older versions
     
     #\ Prime
     dynamicBoost.enable = true;

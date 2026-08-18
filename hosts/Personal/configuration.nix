48527911134c0d92d34ab4b9daff20c7bbd33519
loomhigh <loomhigh223555@gmail.com>
2026-08-18 16:30:23 +0800

    Legacy maxwell I think

--- hosts/Personal/configuration.nix
+++ hosts/Personal/configuration.nix
@@ -36,7 +36,7 @@
   };
 
   # Load nvidia driver for Xorg and Wayland
-  #services.xserver.videoDrivers = lib.mkForce [ "nvidia" ];
+  services.xserver.videoDrivers = lib.mkForce [ "nvidia" ];
  
   hardware.nvidia = {
 
@@ -56,15 +56,15 @@
     # supported GPUs is at: 
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
-    open = true;
+    open = false;
 
     # Enable the Nvidia settings menu,
 	# accessible via `nvidia-settings`.
     nvidiaSettings = false;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-    #package = config.boot.kernelPackages.nvidiaPackages.legacy_535; # Older versions
+    #package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_580; # Older versions
     
     #\ Prime
     dynamicBoost.enable = true;
